ae71aef054f23cfd6669b90a79a41f99e2d224ed
Marlen-Shifu <66721945+Marlen-Shifu@users.noreply.github.com>
2022-11-09 17:10:27 +0600

    fix

--- db/operations.py
+++ db/operations.py
@@ -399,6 +399,8 @@ def create_onetime_task_answer(task_id, user_id, answer,type, value):
 def create_periodic_task_answer(task_id, user_id, answer,type, value):
     try:
 
+        offset = datetime.timedelta(hours=6)
+        timez = datetime.timezone(offset)
 
         ins = PeriodicTaskAnswer(
             task_id = task_id,
@@ -406,7 +408,7 @@ def create_periodic_task_answer(task_id, user_id, answer,type, value):
             answer = answer,
             answer_type = type,
             answer_value = value,
-            time = datetime.datetime.now()
+            time = datetime.datetime.now(tz=timez)
         )
 
         s.add(ins)
