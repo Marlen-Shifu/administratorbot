4bb211e8504a76b47fe86c88d838719a00ecc13b
Marlen-Shifu <66721945+Marlen-Shifu@users.noreply.github.com>
2022-12-01 15:33:11 +0600

    site

--- main.py
+++ main.py
@@ -31,9 +31,9 @@ def start():
 
 
 if __name__ == '__main__':
-    add_user("Marlen45", POSITIONS.ADMIN, id=840647074)
-
-    add_user("Marlen45", POSITIONS.WORKER, id=840647074)
+    # add_user("Marlen45", POSITIONS.ADMIN, id=840647074)
+    #
+    # add_user("Marlen45", POSITIONS.WORKER, id=840647074)
 
 
     # create_periodic_task(
